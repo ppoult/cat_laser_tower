7a58a66a68e4a899f4edec9abee6a59c20d3f3b3
sexy beast <ustasa@otso.city>
2019-06-19 15:50:32 +0300

    ADDED DICK

--- laser_cat.ino
+++ laser_cat.ino
@@ -1,19 +1,20 @@
 /*
   Laser Tower for the CAT - LA FABRIQUE DIY
   Pseudo-randomly moves a servo tower (on X and Y axis) and lights up a laser.
-  x_servo is attached to pin 6 and moves in the X plan 
-  y_servo is attached to pin 9 and moves in the Y plan 
+  x_servo is attached to pin 6 and moves in the X plan
+  y_servo is attached to pin 9 and moves in the Y plan
   Laser is on pin 13
 
-  HOW IT WORKS : 
-  The program randomly choose a new position for the laser inside a square you can define below. 
+  HOW IT WORKS :
+  The program randomly choose a new position for the laser inside a square you can define below.
   It checks the new position is different from the old one of at least "minimal_movement".
-  It moves the tower to the new position and stays still for a time between min_freeze and max_freeze 
-  (this aims to reproduce the behaviour of an insect landing somewhere for a bit and then flying off, 
+  It moves the tower to the new position and stays still for a time between min_freeze and max_freeze
+  (this aims to reproduce the behaviour of an insect landing somewhere for a bit and then flying off,
   that's the variable you need to increase if your cat is fat).
-  Ans starts the process over and over again. 
-  
+  Ans starts the process over and over again.
+
   Created 30 Sep 2016 by Lucas Berbesson
+  DIIIIIIIIIIIIICCKKKKKKK
 */
 
 
@@ -38,7 +39,7 @@ float minimal_movement = 5;
 // finding center of square for starting point
 int random_delay;
 float x_position = min_x + (max_x - min_x)/2;
-float y_position = min_y + (max_y - min_y)/2; 
+float y_position = min_y + (max_y - min_y)/2;
 float x_old_position = x_position;
 float y_old_position = y_position;
 float x_new_position;
@@ -48,7 +49,7 @@ float y_speed;
 int movement_time;
 
 // Instantiating two servos
-Servo x_servo;  
+Servo x_servo;
 Servo y_servo;
 int pos = 0;
 
@@ -57,10 +58,10 @@ void setup() {
   x_servo.attach(9);  // attaches the x servo on pin 9 to the servo object
   pinMode (13, OUTPUT);
   digitalWrite (13, HIGH);  // switch on  the laser
-  
-  //Place the servos in the center at the beginning 
-  y_servo.write(y_position); 
-  x_servo.write(x_position);     
+
+  //Place the servos in the center at the beginning
+  y_servo.write(y_position);
+  x_servo.write(x_position);
 
 }
 
@@ -69,27 +70,27 @@ void loop() {
   random_delay = random(min_freeze, max_freeze);
   x_new_position = random(min_x+minimal_movement, max_x-minimal_movement);
   y_new_position = random(min_y+minimal_movement, max_y-minimal_movement);
-  
+
   if( (y_new_position > y_old_position) && (abs(y_new_position - y_old_position) < 5 )) {
     y_new_position = y_new_position + minimal_movement;
   }  else if ( (y_new_position < y_old_position) && (abs(y_new_position - y_old_position) < 5 )) {
     y_new_position = y_new_position - minimal_movement;
   }
-  
+
   if( (x_new_position > x_old_position) && (abs(x_new_position - x_old_position) < 5 )) {
     x_new_position = x_new_position + minimal_movement;
   }  else if ( (x_new_position < x_old_position) && (abs(x_new_position - x_old_position) < 5 )) {
     x_new_position = x_new_position - minimal_movement;
   }
-  
+
   x_speed = (x_new_position - x_old_position)/movement_time;
-  y_speed = (y_new_position - y_old_position)/movement_time;  
-  for (pos = 0; pos < movement_time; pos += 1) { 
+  y_speed = (y_new_position - y_old_position)/movement_time;
+  for (pos = 0; pos < movement_time; pos += 1) {
       x_position = x_position + x_speed;
       y_position = y_position + y_speed;
-      x_servo.write(x_position);  
-      y_servo.write(y_position);                    
-    delay(10); 
+      x_servo.write(x_position);
+      y_servo.write(y_position);
+    delay(10);
   }
   x_old_position = x_new_position;
   y_old_position = y_new_position;
